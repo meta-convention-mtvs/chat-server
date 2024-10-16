3ae183fb1184615e5c7b75452f20eb15d2195de2
sindydwns <sindydwns@naver.com>
2024-10-17 08:48:05 +0900

    fix: handle realtime-api error

--- srcs/RealtimeAPI.py
+++ srcs/RealtimeAPI.py
@@ -1,6 +1,8 @@
 from fastapi import WebSocket
 import asyncio
 
+ERROR_REQ_PARAM = {"type": "server.error", "code": 4}
+
 class LLMConsole:
     STATUS_WAIT = 0
     STATUS_RUN = 1
@@ -21,21 +23,24 @@ class LLMConsole:
         pass
     
     async def generate(self, type=["text", "audio"]):
+        if self.status != LLMConsole.STATUS_WAIT:
+            await self.ws.send_json({"type": "server.error", "code": 3}) # dup
+            return
         self.generate_type = type
         self.status = LLMConsole.STATUS_RUN
         await asyncio.sleep(0.016)
-        async def send_text(text):
+        async def send_text(text, time=0.016):
             if self.status != LLMConsole.STATUS_RUN or "text" not in self.generate_type:
                 return
             await self.ws.send_json({ "type": "generated.text.delta", "delta": text })
-            await asyncio.sleep(0.016)
-        async def send_audio(file_name):
+            await asyncio.sleep(time)
+        async def send_audio(file_name, time=0.016):
             if self.status != LLMConsole.STATUS_RUN or "audio" not in self.generate_type:
                 return
             with open(file_name) as file:
                 content = file.read()
             await self.ws.send_json({ "type": "generated.audio.delta", "delta": content })
-            await asyncio.sleep(0.016)
+            await asyncio.sleep(time)
         await send_text("메타")
         await send_text(" 컨벤션")
         await send_audio("srcs/sample/pcm_file_0.txt")
@@ -47,7 +52,7 @@ class LLMConsole:
         await send_audio("srcs/sample/pcm_file_3.txt")
         await send_audio("srcs/sample/pcm_file_4.txt")
         await send_audio("srcs/sample/pcm_file_5.txt")
-        await send_text("을")
+        await send_text("을", time=0.5)
         await send_text(" 환영")
         await send_text("합니다")
         await send_text(".")
@@ -56,11 +61,12 @@ class LLMConsole:
             await asyncio.sleep(0.016)
         await send_audio("srcs/sample/pcm_file_6.txt")
         await send_audio("srcs/sample/pcm_file_7.txt")
-        await send_audio("srcs/sample/pcm_file_8.txt")
+        await send_audio("srcs/sample/pcm_file_8.txt", time=1)
         await send_audio("srcs/sample/pcm_file_9.txt")
         if self.status == LLMConsole.STATUS_RUN and "audio" in self.generate_type:
             await self.ws.send_json({ "type": "generated.audio.done" })
             await asyncio.sleep(0.016)
+        self.status = LLMConsole.STATUS_WAIT
 
     async def cancel(self):
         if self.status == LLMConsole.STATUS_WAIT:
