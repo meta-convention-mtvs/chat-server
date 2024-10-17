0f109297b7e8d54d6e8061f1784afc6b0a3cbe89
sindydwns <sindydwns@naver.com>
2024-10-18 01:00:19 +0900

    feat: add openai-realtimeapi

--- srcs/RealtimeAPI.py
+++ srcs/RealtimeAPI.py
@@ -1,5 +1,8 @@
 from fastapi import WebSocket
+import websockets
 import asyncio
+import json
+import os
 
 ERROR_REQ_PARAM = {"type": "server.error", "code": 4}
 
@@ -11,23 +14,74 @@ class LLMConsole:
         self.ws = ws
         self.org = org
         self.status = LLMConsole.STATUS_WAIT
-        self.generate_type = None
+        self.modalities = None
+        self.log("created")
+    
+    async def load(self):
+        url = "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01"
+        headers = {
+            "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}",
+            "OpenAI-Beta": "realtime=v1",
+        }
+        self.ai = await websockets.connect(url, extra_headers=headers)
+        self.log("connected")
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.onmessage())
     
     async def add_audio(self, buffer):
+        self.ai.send
         pass
     
     async def clear_audio(self):
         pass
     
     async def add_text(self, text):
+        # TODO
         pass
     
-    async def generate(self, type=["text", "audio"]):
+    async def generate(self, modalities=["text", "audio"]):
         if self.status != LLMConsole.STATUS_WAIT:
             await self.ws.send_json({"type": "server.error", "code": 3}) # dup
             return
-        self.generate_type = type
+        self.modalities = modalities
         self.status = LLMConsole.STATUS_RUN
+        await self.ai.send(json.dumps({ "type": "response.create", "response": {"modalities": modalities} }))
+
+    async def cancel(self):
+        if self.status == LLMConsole.STATUS_WAIT:
+            return
+        self.status = LLMConsole.STATUS_WAIT
+        if "text" in self.modalities:
+            self.ws.send_json({ "type": "generated.text.canceled" })
+        if "audio" in self.modalities:
+            self.ws.send_json({ "type": "generated.audio.canceled" })
+
+    async def onmessage(self):
+        while True:
+            raw = await self.ai.recv()
+            data = json.loads(raw)
+            if data.get("type") == "response.audio_transcript.delta" or data.get("type") == "response.text.delta":
+                await self.ws.send_json({ "type": "generated.text.delta", "delta": data.get("delta") })
+                print("generated.text.delta:", data.get("delta"), flush=True)
+            elif data.get("type") == "response.audio_transcript.done" or data.get("type") == "response.text.done":
+                await self.ws.send_json({ "type": "generated.text.done", "text": data.get("transcript", data.get("text")) })
+                print("generated.text.done:", data.get("transcript", data.get("text")), flush=True)
+            elif data.get("type") == "response.audio.delta":
+                await self.ws.send_json({ "type": "generated.audio.delta", "delta": data.get("delta") })
+                print("generated.audio.delta:", str(len(data.get("delta"))), flush=True)
+            elif data.get("type") == "response.audio.done":
+                await self.ws.send_json({ "type": "generated.audio.done" })
+                print("generated.audio.done:", data.get("audio done!"), flush=True)
+            elif data.get("type") == "response.done":
+                self.status = LLMConsole.STATUS_WAIT
+                print("response.done", flush=True)
+            elif data.get("type") == "error":
+                print("error:", data.get("error"), flush=True)
+            else:
+                print("ect:", data, flush=True)
+                
+                
+        self.log("test")
         await asyncio.sleep(0.016)
         async def send_text(text, time=0.016):
             if self.status != LLMConsole.STATUS_RUN or "text" not in self.generate_type:
@@ -56,23 +110,18 @@ class LLMConsole:
         await send_text(" 환영")
         await send_text("합니다")
         await send_text(".")
-        if self.status == LLMConsole.STATUS_RUN and "text" in self.generate_type:
+        if self.status == LLMConsole.STATUS_RUN and "text" in self.modalities:
             await self.ws.send_json({ "type": "generated.text.done" })
             await asyncio.sleep(0.016)
         await send_audio("srcs/sample/pcm_file_6.txt")
         await send_audio("srcs/sample/pcm_file_7.txt")
         await send_audio("srcs/sample/pcm_file_8.txt", time=1)
         await send_audio("srcs/sample/pcm_file_9.txt")
-        if self.status == LLMConsole.STATUS_RUN and "audio" in self.generate_type:
+        if self.status == LLMConsole.STATUS_RUN and "audio" in self.modalities:
             await self.ws.send_json({ "type": "generated.audio.done" })
             await asyncio.sleep(0.016)
         self.status = LLMConsole.STATUS_WAIT
 
-    async def cancel(self):
-        if self.status == LLMConsole.STATUS_WAIT:
-            return
-        self.status = LLMConsole.STATUS_WAIT
-        if "text" in self.generate_type:
-            self.ws.send_json({ "type": "generated.text.canceled" })
-        if "audio" in self.generate_type:
-            self.ws.send_json({ "type": "generated.audio.canceled" })
+    def log(self, s):
+        with open("srcs/conn.log", "+a") as file:
+            file.write(s + "\n")
--- srcs/main.py
+++ srcs/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 import uvicorn
 import json
+import dotenv
 from .RealtimeAPI import LLMConsole
+
+dotenv.load_dotenv()
 
 ERROR_FATAL = {"type": "server.error", "code": 1}
 ERROR_NO_ORG = {"type": "server.error", "code": 2}
@@ -23,6 +26,7 @@ async def websocket_endpoint(websocket: WebSocket):
                 await websocket.send_json(ERROR_REQ_PARAM)
             else:
                 llm = LLMConsole(websocket, org_id)
+                await llm.load()
 
         elif message["type"] == "buffer.add_audio":
             audio_data = message.get("audio")
