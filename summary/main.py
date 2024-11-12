a8be230bcb01af3b10c42c8486326daba01806a9
yeonwlee <81205766+yeonwlee@users.noreply.github.com>
2024-11-12 19:24:28 +0900

    Fix/handle log exception (#34)
    * fix: handle invalid log file
    
    * fix: modify handling exception position
    
    * fix: modify foolish logging
    
    * Update summary/service/summary.py
    
    Co-authored-by: sindy <sindydwns@naver.com>
    
    ---------
    
    Co-authored-by: sindy <sindydwns@naver.com>

--- summary/main.py
+++ summary/main.py
@@ -14,15 +14,14 @@ async def hello():
 
 @app.post("/summary")
 async def summary(userinfo:BuyerAIConversationSummaryRequest) -> dict:
-    try:
-        return exec_summary(userinfo)
-    except Exception as e:
-        logging.error(e)
+    result = exec_summary(userinfo)
+    if not result:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="유저가 해당 기업의 AI직원과 대화한 기록을 찾을 수 없음",
-        )
-
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="유저가 해당 기업의 AI직원과 대화한 기록을 찾을 수 없음",
+    )
+    return result 
+        
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
