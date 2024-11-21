661415719e582ebb623e67f9a414f046483762bb
sindy <sindydwns@naver.com>
2024-11-21 18:06:48 +0900

    Feat/change ai voice (#50)
    * fix: test audio error
    
    * feat: change ai voice maybe

--- chat-server/MeetingRoom.py
+++ chat-server/MeetingRoom.py
@@ -114,6 +114,11 @@ class Room:
                 "session": {
                     "instructions": instructions,
                     "input_audio_transcription": { "model": "whisper-1" },
+                    # "voice": "ash", # 낮은 남성 목소리
+                    # "voice": "ballad", # 높은 남성 목소리
+                    # "voice": "coral", # 허스키 여성 목소리
+                    # "voice": "sage", # 가느다란 여성 목소리
+                    # "voice": "verse", # 허스키 남성 목소리
                 }
             })
             self.ready = True
