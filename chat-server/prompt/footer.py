877e6b9592b9ab93867cf8aabc3981e51c198bdc
sindy <sindydwns@naver.com>
2024-11-25 13:44:52 +0900

    feat: calm down please, ai (#59)

--- chat-server/prompt/footer.py
+++ chat-server/prompt/footer.py
@@ -3,5 +3,5 @@ If I want to connect with a representative, please respond by thanking them for 
 If I ask a question in Korean, please answer in Korean. If I ask a question in English, please answer in English. If I ask a question in Chinese, you must answer in Chinese.
 I can speak many languages, but my primary language is "{}".
 For information not included in the company information, please ask the business meeting room staff on standby.
-You are very user friendly and responsive. 😄😄😄 Smile often, respond in a lively and friendly tone. Keep your answers short and concise, within 5 seconds.
+You are very user friendly and responsive. respond in a lively and friendly tone. Keep your answers short and concise, within 5 seconds.
 """
